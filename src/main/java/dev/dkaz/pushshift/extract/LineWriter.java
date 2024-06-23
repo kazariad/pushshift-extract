e4560351fb6deedbb601a2ce0b897733457714a0
kazariad <122331465+kazariad@users.noreply.github.com>
2024-06-23 05:00:57 -0700

    Delete output file if nothing written

--- src/main/java/dev/dkaz/pushshift/extract/LineWriter.java
+++ src/main/java/dev/dkaz/pushshift/extract/LineWriter.java
@@ -3,35 +3,40 @@ package dev.dkaz.pushshift.extract;
 import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
 import java.nio.file.StandardOpenOption;
 
 public class LineWriter implements Runnable {
     @Override
     public void run() {
-        ByteBuffer outBuf = ByteBuffer.allocate(1024 * 1024 * 16);
-        byte[] LF = "\n".getBytes(StandardCharsets.UTF_8);
-        boolean isFirstLine = true;
+        try {
+            ByteBuffer outBuf = ByteBuffer.allocate(1024 * 1024 * 16);
+            byte[] LF = "\n".getBytes(StandardCharsets.UTF_8);
+            boolean isFirstLine = true;
 
-        try (FileChannel outChannel = FileChannel.open(Args.outputPath, StandardOpenOption.CREATE_NEW, StandardOpenOption.WRITE)) {
-            for (String line = Main.WRITER_QUEUE.take(); line != Main.EOF; line = Main.WRITER_QUEUE.take()) {
-                if (isFirstLine) {
-                    isFirstLine = false;
-                } else {
-                    outBuf.put(LF);
-                }
-                outBuf.put(line.getBytes(StandardCharsets.UTF_8));
+            try (FileChannel outChannel = FileChannel.open(Args.outputPath, StandardOpenOption.CREATE_NEW, StandardOpenOption.WRITE)) {
+                for (String line = Main.WRITER_QUEUE.take(); line != Main.EOF; line = Main.WRITER_QUEUE.take()) {
+                    if (isFirstLine) {
+                        isFirstLine = false;
+                    } else {
+                        outBuf.put(LF);
+                    }
+                    outBuf.put(line.getBytes(StandardCharsets.UTF_8));
 
-                outBuf.flip();
-                while (outBuf.hasRemaining()) {
-                    outChannel.write(outBuf);
+                    outBuf.flip();
+                    while (outBuf.hasRemaining()) {
+                        outChannel.write(outBuf);
+                    }
+                    outBuf.clear();
                 }
-                outBuf.clear();
             }
+
+            if (isFirstLine) Files.delete(Args.outputPath);
+
+            ProgressMonitor.isFinished.set(true);
         } catch (Exception e) {
             System.err.println(e.toString());
             System.exit(1);
         }
-
-        ProgressMonitor.isFinished.set(true);
     }
 }
